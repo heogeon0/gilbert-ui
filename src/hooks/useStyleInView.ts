8fb66b3c11312eb6259a30152a3f506b637d4013
heogeon0 <heo3793@naver.com>
2025-04-21 23:14:40 +0900

    feat: useViewportRect.tsx 훅 개발

---
+++ src/hooks/useStyleInView.ts
@@ -0,0 +1,5 @@
+// type PositionKey = 'left' | 'right' | 'top' | 'bottom'
+// type Positions = Partial<Record<PositionKey, string | number>>
+// type Style = Partial<Record<'left' | 'right' | 'top' | 'bottom', number>>
+//
+// const useStyleInView = () => {}
