90e5643d8c34953964ac9239e7be3c0d474a8887
heogeon0 <heo3793@naver.com>
2025-05-31 19:03:58 +0900

    feat: 스크롤 박스 스토리 추가

---
+++ src/headless-ui/_stories/scrollBox.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ComponentType } from 'react'
+
+import LazyImage from '../lazyImage'
+
+import ScrollBox, { ScrollBoxProps } from '@/headless-ui/scrollBox'
+
+const meta = {
+  title: 'Headless-ui/ScrollBox',
+  component: ScrollBox as unknown as ComponentType<
+    ScrollBoxProps<{ id: string }>
+  >,
+  decorators: [
+    (Story) => (
+      <div style={{ height: '30vh', width: '100%', overflow: 'hidden' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+} satisfies Meta<typeof ScrollBox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+  args: {
+    customButtonProps: {
+      useButton: true,
+    },
+    ref: null,
+    renderer: (index) => (
+      <LazyImage
+        width={50}
+        height={100}
+        src={`https://picsum.photos/50/100?random=${index}`}
+      />
+    ),
+    itemCount: 30,
+  },
+}
