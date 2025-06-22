c493a879e45b1d05c59b2a732fa2af80cf9346d8
heogeon0 <heo3793@naver.com>
2025-06-22 21:58:29 +0900

    feat: 팝오버 위치 조정 기능 구현
    - 팝오버 위치를 viewport에 맞게 조정하는 로직 구현 (src/hooks/useStyleInView.ts)
    - positionType에 따라 absolute, relative 위치 설정
    - 팝오버 업데이트 여부에 따른 업데이트 기능 구현

--- src/hooks/useStyleInView.ts
+++ src/hooks/useStyleInView.ts
@@ -1,59 +1,77 @@
 import { RefObject, useLayoutEffect, useState } from 'react'
 
-import useCheckIsChildRenderedObserver from '@/hooks/useMutationObeserver.ts'
-import useViewportRect from '@/hooks/useViewportRect.tsx'
+import useViewportRect from '@/hooks/useViewportRect'
 
-type PositionKey = 'left' | 'right' | 'top' | 'bottom'
-type Positions = Partial<Record<PositionKey, string | number>>
+type PositionKey = 'left' | 'right' | 'top' | 'bottom'  
+type Position = Partial<Record<PositionKey, string | number>>
 type Style = Partial<Record<'left' | 'right' | 'top' | 'bottom', number>>
 
-/**
- * 렌더링될 요소(ex: popover, tooltip 등)의 위치를 viewport에 맞게 조정하는 hook
- * @param wrapperRef - 렌더링될 요소의 부모 엘리먼트
- * @param observeRef - 자식 변화를 탐지할 엘리먼트
- * @param targetId - 렌더링될 요소의 id
- * @param position - 렌더링될 요소의 위치
- * @returns style - 렌더링될 요소의 위치
- * */
 const useStyleInView = (
+  /** 팝오버 랩퍼 영역 */
   wrapperRef: RefObject<HTMLElement | null>,
-  observeRef: RefObject<HTMLElement | null>,
-  targetId: string,
-  position: Positions
+  /** 팝오버 타겟 영역(팝오버를 열게하는 버튼) */
+  targetRef: RefObject<HTMLElement | null>,  
+  /** 팝오버 위치 */
+  position: Position,
+  /** 팝오버 위치 타입 */
+  positionType: 'absolute' | 'relative' = 'relative',
+  /** 팝오버 업데이트 여부 */
+  needUpdate: boolean = true,
 ) => {
   const viewportRect = useViewportRect()
-  const { isRendered, targetElement } = useCheckIsChildRenderedObserver(
-    observeRef,
-    targetId
-  )
-
-  const [style, setStyle] = useState<Style>()
+  const [style, setStyle] = useState<Style>({})
 
   useLayoutEffect(() => {
-    if (!wrapperRef.current || !targetElement) return
-
+    if (!needUpdate || !wrapperRef.current || !targetRef.current) return
     const wrapperRect = wrapperRef.current.getBoundingClientRect()
-    const targetRect = targetElement.getBoundingClientRect()
+    const targetRect = targetRef.current.getBoundingClientRect()
 
-    // verticalKey의 의미는 top인 경우 렌더링 할 요소(tagetRef)의 top 위치를 결정한다는 의미임
+    console.log(viewportRect, targetRect.top)
+
+    // "기준값". top은 top을 기준으로 아래로 보여주기. bototm은 위로 보여주기.
     const verticalKey =
-      wrapperRect.bottom + targetRect.height < viewportRect.height
-        ? 'top'
-        : 'bottom'
+      targetRect.bottom + wrapperRect.height < viewportRect.height ? 'top' : 'bottom'
     const horizontalKey =
-      wrapperRect.right + targetRect.width < viewportRect.width
-        ? 'left'
-        : 'right'
+      targetRect.right + wrapperRect.width < viewportRect.width ? 'left' : 'right'
 
-    console.log('ver', verticalKey)
 
-    setStyle({
-      [verticalKey]: position[verticalKey] || 0,
-      [verticalKey === 'top' ? 'bottom' : 'top']: 'auto',
-      [horizontalKey]: position[horizontalKey] || 0,
-      [horizontalKey === 'left' ? 'right' : 'left']: 'auto',
-    })
-  }, [viewportRect, isRendered, position])
+      console.log(verticalKey, horizontalKey)
+    /* console.log({
+      horizontalKey,
+      wrapperRight: wrapperRect.right,
+      targetWidth: targetRect.width,
+      viewportWidth: viewportRect.width,
+    }) */
+
+    if (positionType === 'absolute') {
+      const absoluteTop = -viewportRect.top + targetRect.top
+
+
+      console.log(absoluteTop)
+
+      setStyle({
+        [verticalKey]:
+          verticalKey === 'top'
+            ? absoluteTop + targetRect.height + +(position.top || 0)
+            : viewportRect.height - absoluteTop + +(position.bottom || 0),
+        [verticalKey === 'top' ? 'bottom' : 'top']: 'auto',
+        [horizontalKey]:
+          horizontalKey === 'left'
+            ? targetRect.left - +(position.left || 0)
+            : viewportRect.width - targetRect.right + +(position.right || 0),
+        [horizontalKey === 'left' ? 'right' : 'left']: 'auto',
+      })
+      
+    } else {
+      /* relative이던 기존방식:  */
+      setStyle({
+        [verticalKey]: position[verticalKey] || 0,
+        [verticalKey === 'top' ? 'bottom' : 'top']: 'auto',
+        [horizontalKey]: position[horizontalKey] || 0,
+        [horizontalKey === 'left' ? 'right' : 'left']: 'auto',
+      })
+    }
+  }, [viewportRect, wrapperRef, targetRef, position, needUpdate])
 
   return style
 }
